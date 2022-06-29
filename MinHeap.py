f423d603a610c9e80f757d7aa4ff41064260bfc5
rashupee <rashupee@gmail.com>
2022-06-28 18:51:18 -0700

    Now have a working sorter based partially on min_heap concept

--- MinHeap.py
+++ MinHeap.py
@@ -4,37 +4,54 @@
 
 # careful to not alter the original list
 
-original_list = [10, 121, 94, 3, -7, 203, 45, 93, 27, -35, 6, 1, -7]
+# An unordered list to work with
+# original_list = [10, 121, 94, 3, -7, 203, 45, 93, 27, -35, 6, 1, -7]
 
-min_heap = []
+
+def parent_index(child_index):
+	# For dense binary tree, returns index of parent associated with child_index
+
+	return (child_index - 1) // 2
+
 
 
 def swap_elements(array, index_1, index_2):
 	# Question: will manipulations of array lead to a change in a more global variable. I think so- lists
 	# 	are mutable
 
-	high, low = max(index_1, index_2), min(index_1, index_2)
-
-	array.insert( low, array.pop( high ))
-	array.insert( high, array.pop( low + 1 ))
-
+	array[index_1], array[index_2] = array[index_2], array[index_1]
 	return array
 
-def initialized_min_heap(array):
-	# Returns the beginning of a min_heap
-	# returns a list where the first element is the smallest value, and the rest of the elements
-	# are simply the unsorted rest of the original list
 
-	min_heap = []
-	min_heap.append(min(array))
 
-	index_to_skip = array.index(min_heap[0])
+def float_min_to_top(array):
+	# Moves minimum element into the root position (index zero in the list)
+	# The rest of the elements may be shuffled but this is not a min_heap, in general.
 
-	for index in range(len(array)):
-		if not index = index_to_skip:
-			min_heap.append(array[index])
+	for index in range(len(array)-1, 0, -1):
+		parent = parent_index(index)
+		if array[parent] > array[index]:
+			swap_elements(array, index, parent)
+	return array
 
-	return min_heap
+
+def sorted(array):
+	answer=[]
+	for layer in range(len(array)-1, -1, -1):
+		answer.append(float_min_to_top(array)[0])
+		array[0] = array[len(array)-1]
+		array.pop()
+
+	return answer
+
+
+
+
+
+
+
+
+
 
 
 
